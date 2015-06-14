bc6717e3810a0919e8de9ae5a482bdbb639806e7
murum <cr222cs@student.lnu.se>
2015-06-14 18:51:31 +0200

    Added new assets

---
+++ resources/assets/js/app.js
@@ -0,0 +1,6 @@
+$(function() {
+    $("html").fitText(3.2, {
+        minFontSize: "11px",
+        maxFontSize: "16px"
+    });
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5 - Laravings</div>
                 <div class="quote">{{ Inspiring::quote() }}</div>
             </div>
         </div>
