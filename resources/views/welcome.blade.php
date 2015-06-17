c98b82d36a73248ed5cd1f1b400677a5711dd12d
murum <cr222cs@student.lnu.se>
2015-06-17 10:40:43 +0200

    Added views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,52 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends("layout.master")
 
-        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-                margin-bottom: 40px;
-            }
-
-            .quote {
-                font-size: 24px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-                <div class="quote">{{ Inspiring::quote() }}</div>
-            </div>
+@section("content")
+<div class="container">
+    <div class="content">
+        <div class="title">
+            <h1>
+                Laravel 5 - Larawings
+            </h1>
         </div>
-    </body>
-</html>
+
+        <div class="quote">{{ Inspiring::quote() }}</div>
+    </div>
+</div>
+@stop
